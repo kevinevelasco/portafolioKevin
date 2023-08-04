c5d070fdcca785a616577a2996eb6c5170be1a21
kevinevelasco <90013072+kevinevelasco@users.noreply.github.com>
2023-08-04 19:56:46 +0000

    Commit inicial

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Kevin Estiven Velasco Pinto",
+  title: "Estudiante de Ingeniería de Sistemas",
+  email: "kevinvelasco110228lsb@gmail.com",
+  gitHub: "kevinevelasco",
+  instagram: "kevinevelasco",
+  linkedIn: "kevinevelasco",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
